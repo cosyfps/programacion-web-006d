9268d41d1557fc7d7afb27e21a330f169372519d
RayCL2 <bast.bascunan@duocuc.cl>
2024-05-28 20:14:04 -0400

    models update

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -36,3 +36,8 @@ class Libro(models.Model):
         verbose_name = "libro"
         verbose_name_plural = "libros"
 
+class Users(models.Model):
+    nombreUser = models.CharField(max_length=100, null = False, blank=False)
+    emailUser = models.CharField(max_length=20, null=False,blank=False)
+    passUser = models.CharField(max_length=20, null=False,blank=False )
+    
