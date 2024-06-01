62e2baf5b153ab28df2667060bec4d6f0188d1fa
cosy <kelvin.morenog28@gmail.com>
2024-05-31 23:12:37 -0400

    Creacion de Frontend

--- myproject/myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics,permissions
-from django.contrib.auth.models import User
-from .models import Users
-from .serializers import UserSerializer
-# Create your views here.
